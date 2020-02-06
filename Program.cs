b94bcedc64649dddcdba605e198aa664a56d19d4
Roger Dominguez <rogerd@mail.usf.edu>
2020-02-06 02:24:42 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿//Roger Dominguez
+// 2/6/2020
+// Array that executes 25 elements
+using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                const int Size = 26;
+
+                int[] countdown = new int[Size];
+                Console.WriteLine("");
+                int pop_array = 0;
+                for (int i = 0; i < Size; i++)
+                {
+                    countdown[i] = pop_array;
+                    Console.WriteLine(" Element value = " + i);
+                }
+                Console.WriteLine("");
+                Console.ReadKey(true);
+            }
+
+
+            catch
+            {
+                Console.WriteLine("Press any key to exit ...");
+                Console.ReadKey(true);
+            } 
+            }   
+        }      
+    }
+
+
+            
